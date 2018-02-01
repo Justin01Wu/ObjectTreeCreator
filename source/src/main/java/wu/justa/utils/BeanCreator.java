6b2313e4646579cd942fed1cac21d1826db158c2
Justin Wu <justin01.wu@gmail.com>
2018-02-01 14:37:50 -0500

    improve Java docs

--- source/src/main/java/wu/justa/utils/BeanCreator.java
+++ source/src/main/java/wu/justa/utils/BeanCreator.java
@@ -1,5 +1,11 @@
 package wu.justa.utils;
 
+/**
+ * 
+ * @author justin.wu
+ *
+ * @param <T>
+ */
 public interface BeanCreator<T> {
 	
 	public T createBean();
--- source/src/main/java/wu/justa/utils/BeanGenerator.java
+++ source/src/main/java/wu/justa/utils/BeanGenerator.java
@@ -16,6 +16,151 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Stack;
 
+/**
+ * 
+ * <h1>ObjectTreeCreator</h1>
+ *  
+ *   
+ <h2>What is it? </h2>
+<pre>
+This util automatically walk through the class tree on Java reflection and create an object tree with default values
+What does it do?
+
+Because Java has so many types of class, this code only covers most common Java class, like
+
+    all primitive types and its wrapper (Integer default value will be 12345, and so on)
+    List, Set and Map (will create one element)
+    Array (will create one element)
+    String (default value is "a string")
+    enum (will use first item)
+    and so on
+
+The process will walk through the class tree: call default constructor and run all setter on it.
+
+If an exception happen, the related properties will be set to null, you can customize it to avoid those exceptions.
+
+You can see some samples below.
+</pre>
+
+
+<h2>How to use it</h2>
+<pre>
+* {@code
+	@Test
+	public void testIntegerCreator() throws Exception {
+		BeanGenerator generator = new BeanGenerator();		
+		Integer result = generator.generate(Integer.class);
+		assertEquals(result, new Integer(12345));
+	}
+* }
+</pre>
+
+<h2>Customizing it</h2>
+
+User can customize it by adding their own creator:
+<pre>
+{@code
+	public class CustomizedBeanCreator implements BeanCreator<CustomizedBean>{
+		
+		@Override
+		public CustomizedBean createBean() {
+			CustomizedBean bean = new CustomizedBean();
+			bean.setId(-2323);
+			bean.setName("this bean is created by a customized creator");		
+			return bean;
+		}
+	
+		@Override
+		public Class<CustomizedBean> getClazz() {
+			return CustomizedBean.class;
+		}
+	}
+}
+</pre>
+
+Now you can register it :
+<pre>
+{@code
+	@Test
+	public void testCustomizedCreator() throws Exception {
+		BeanGenerator generator = new BeanGenerator();
+		generator.addExternalCreator(new CustomizedBeanCreator());
+		CustomizedBean result = generator.generate(CustomizedBean.class);
+		System.out.println(result);
+		assertEquals(result.getId(), new Integer(-2323));
+		assertEquals(result.getName(), "this bean is created by a customized creator");
+
+	}
+}
+</pre>
+
+You can also customize an existing creator, like primitive type
+
+<pre>
+{@code
+	public class CustomizedStringCreator implements BeanCreator <String>{
+		
+		@Override
+		public String createBean() {
+			String bean = new String("===this is customized String===");
+			return bean;
+		}
+	
+		@Override
+		public Class<String> getClazz() {
+			return String.class;
+		}
+	}
+}
+</pre>
+
+<h2>Error handling</h2>
+<pre>
+If a class in all of those situations:
+
+    it is not covered by default handlers
+    it is not customized,
+    and it doesn't have default constructor
+
+then the util will set it to null
+
+If any exceptions occur during those process, the value will be set to null
+
+It can detect nested class:
+
+{@code
+	public class NestedBean {
+		private NestedBean parent;
+		public NestedBean getParent() {
+			return parent;
+		}
+		public void setParent(NestedBean parent) {
+			this.parent = parent;
+		}
+	}
+}
+
+Here parent will be set to null to avoid infinite loop.
+
+<h2>Why do I create this util?</h2>
+
+
+We had many RESTful API On Jackson, I created a java doclet on it to get RESTful API docs. 
+But I had a difficulty to prepare sample for it.
+
+I did some research on the marketing, and didn't find a good solution for it.
+
+So I decided to create this tool to automatically create a java object tree based on its class.
+
+<h2>Dependency</h2>
+
+This Jar won't depend on any Third party library except JDK 7 and above.
+
+</pre>
+
+ * @author justin.wu
+ *
+ */
 public class BeanGenerator {
 	private static Date NOW = new Date();
 	
