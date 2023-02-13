dea9b6c22200fd53c9dfe0b01ae6a7d5efbc4f24
Justin.Wu <justin.wu@validusholdings.com>
2023-02-13 15:49:09 -0500

    improve byte-buddy samples

--- source/src/test/java/wu/justa/utils/GetterProxyTest.java
+++ source/src/test/java/wu/justa/utils/GetterProxyTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Calendar;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -20,6 +21,7 @@ import wu.justa.utils.bean.BeanExtended;
 public class GetterProxyTest {
 
 	@Test
+	@Ignore("JDK builtIn proxy need interface, which is not easy for existing old code" )
 	public void testInvoke() throws JsonProcessingException {
 		GetterProxy proxy = new GetterProxy(Address.class.getInterfaces());
 		Address x = BeanGenerator2.newProxyInstance(Address.class, proxy);
@@ -33,13 +35,14 @@ public class GetterProxyTest {
 
 	}
 
+	// byteBuddy sample
 	@Test
 	public void testAddress() throws JsonProcessingException, InstantiationException, IllegalAccessException {
 
 		DynamicType.Unloaded unloadedType = new ByteBuddy().subclass(Address.class)
 				.method(ElementMatchers.isGetter().and(returns(String.class)))
 				.intercept(FixedValue.value("Hello World ByteBuddy!"))
-				.method(ElementMatchers.isGetter().and(returns(Integer.class)))
+				.method(ElementMatchers.isGetter().and(returns(int.class)))
 				.intercept(FixedValue.value(12345)).make();
 
 		Class<Address> dynamicType = unloadedType.load(Address.class.getClassLoader()).getLoaded();
@@ -57,12 +60,21 @@ public class GetterProxyTest {
 	
 	@Test
 	public void testBeanExtended() throws JsonProcessingException, InstantiationException, IllegalAccessException {
+		byte b = 123;
+		short s = 123;
+		float f = 123.45f;
 
 		DynamicType.Unloaded unloadedType = new ByteBuddy().subclass(BeanExtended.class)
 				.method(ElementMatchers.isGetter().and(returns(String.class)))
 				.intercept(FixedValue.value("Hello World ByteBuddy!"))
 				.method(ElementMatchers.isGetter().and(returns(Integer.class)))
 				.intercept(FixedValue.value(12345))
+				.method(ElementMatchers.isGetter().and(returns(short.class)))
+				.intercept(FixedValue.value(s))
+				.method(ElementMatchers.isGetter().and(returns(byte.class)))
+				.intercept(FixedValue.value(b))
+				.method(ElementMatchers.isGetter().and(returns(float.class)))
+				.intercept(FixedValue.value(f))				
 				.method(ElementMatchers.isGetter().and(returns(Long.class)))
 				.intercept(FixedValue.value(1234567l))
 				.method(ElementMatchers.isGetter().and(returns(Boolean.class)))
